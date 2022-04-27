a4c1938a4d3f874b34352d1286e1f9ef29427c41
안영선 <hi0seon@an-yeongseon-ui-MacBookPro.local>
2022-04-28 00:40:22 +0900

    day01-2

---
+++ day01/신고결과받기.py
@@ -0,0 +1,35 @@
+def solution(id_list, report, k):
+	answer = []
+	check = {}
+	count = {}
+	for i in range(len(id_list)):
+		check[id_list[i]] = []
+		count[id_list[i]] = 0
+		answer.append(0)
+	
+	for i in report:
+		# print(i.split()) ['muzi', 'frodo']
+		rep = i.split()
+		issame = False
+		for j in check[rep[0]]:
+			if j == rep[1]:
+				issame = True
+		if not issame:
+			count[rep[1]] += 1
+			check[rep[0]].append(rep[1])
+	print(check)
+	for i in count:
+		if count[i] >= k:
+			for j, lst in check.items():
+				for m in lst: # j 가 신고한 목록 lst
+					if i == m: # 신고당한 횟수 k 이상인 i가 신고한 목록에 있으면
+						answer[id_list.index(j)] += 1
+	return answer
+
+print(solution(["muzi", "frodo", "apeach", "neo"], 
+["muzi frodo","apeach frodo","frodo neo","muzi neo",
+"apeach muzi"],2))
+
+print(solution(["con", "ryan"], 
+["ryan con", "ryan con", "ryan con", "ryan con"], 
+3))
