9e9cbc5cb8d0f28d55670a234a67f888c237fb55
Ahn YoungSeon <dudtjs0920@naver.com>
2022-05-10 20:17:06 +0900

    주석 추가

--- day09/greedy_조이스틱.py
+++ day09/greedy_조이스틱.py
@@ -1,3 +1,5 @@
+# 시작 위치는 항상 맨 앞
+
 def solution(name):
     answer = 0
     alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -11,8 +13,8 @@ def solution(name):
             next += 1
         min_move = min([
                 min_move, 
-                2 * cur + len(name) - next, \
-                cur + 2 * (len(name) - next)])
+                2 * cur + len(name) - next, # A 앞의 원소들을 돌고 뒤로 넘어갈 것인지
+                cur + 2 * (len(name) - next)]) # A 뒤의 원소들을 먼저 돌고 앞으로 넘어올 것인지
         cur += 1
     return min_move + answer
 
